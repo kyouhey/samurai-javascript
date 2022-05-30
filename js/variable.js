2bb5cff0e4159ac5eef2a04db8b789f2926ed999
Kyohei Yonekura <kyoheiyonekura@KyoheinoMacBook-Air.local>
2022-05-30 19:19:35 +0900

    first commit

---
+++ js/variable.js
@@ -0,0 +1,20 @@
+// 変数の宣言
+let userNome;
+
+// 値の代入
+userNome = '侍太郎';
+
+// 変数の宣言・値の代入
+let userNumber = 55;
+
+// コンソールへの出力
+console.log(userNome);
+console.log(userNumber);
+
+//値の再代入
+userNome = '侍花子';
+userNumber = 88;
+
+//コンソールへの出力
+console.log(userNome);
+console.log(userNumber);
